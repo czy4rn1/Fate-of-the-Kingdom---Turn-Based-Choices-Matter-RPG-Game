fc1ac9f834ae687961e3a08a58dc25833819b3b5
czy4rn1 <michal.czarnobaj@wp.pl>
2026-07-02 15:34:07 +0200

    Added CastleMain level

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,8 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public string sceneName;
+    
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            LoadScene(sceneName);
+        }
     }
 }
