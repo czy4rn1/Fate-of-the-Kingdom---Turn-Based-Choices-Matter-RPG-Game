34564f5f4bd4e888f7f512379c9a27e219630ff8
CZy4Rn1 <michal.czarnobaj@wp.pl>
2026-08-08 17:14:38 +0200

    Added transition from Volsen to VolsenInn

---
+++ Assets/Scripts/EnterArea.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterArea : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+    public Player player;
+    public PlayerDetection playerDetection;
+    private bool interactionActive = false;
+    public string sceneName;
+    public BlackoutManager blackoutManager;
+
+    
+    void Update()
+    {
+        if (interactionActive)
+        {
+            playerDetection.allowIcon = false;
+        }
+        else {playerDetection.allowIcon = true;}
+        if (!interactionActive)
+        {
+            if (!dialogueManager.dialogueActive &&
+            player.isControllable && 
+            playerDetection.isPlayerNearby && 
+            Input.GetKeyDown(KeyCode.F))
+            {
+                interactionActive = true;
+                player.isControllable = false;
+
+                dialogueManager.ShowDialogue("Do you want to enter the area?\n1. Yes\n2. No", false, 2, true, OnChosenCommand);
+            }
+        }
+    }
+
+    public void OnChosenCommand(int command)
+    {
+        if (command == 0)
+        {
+            StartCoroutine(LoadArea());
+        }
+        else if (command == 1)
+        {
+            CloseDialogue(0);
+        }
+    }
+    public void CloseDialogue(int nothing)
+    {
+        player.isControllable = true;
+        interactionActive = false;
+    }
+
+    public IEnumerator LoadArea()
+    {
+        yield return blackoutManager.Fade(false);
+        while (blackoutManager.curAlpha < 1f) yield return null;
+        SceneManager.LoadScene(sceneName);
+    }
+}
