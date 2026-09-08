70b6abb0648286ab9af25a9b6859124af408296b
CZy4Rn1 <michal.czarnobaj@wp.pl>
2026-09-08 12:40:50 +0200

    Universal functionality to MoveOnTouch

--- Assets/Scripts/DungeonLava/MoveOnTouch.cs
+++ Assets/Scripts/DungeonLava/MoveOnTouch.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class MoveOnTouch : MonoBehaviour
 {
     public Player player;
+    public float newX;
+    public float newY;
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) player.transform.position = new Vector2(-8.56f, -0.82f);
+        if (collision.CompareTag("Player")) player.transform.position = new Vector2(newX, newY);
     }
 }
